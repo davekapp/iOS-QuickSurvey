66f679daed653a10c11bde3d48347458689ad62c
David Kapp <davekapp@gmail.com>
2012-02-08 20:43:10 -0700

    Initial Commit

---
+++ QuickSurveyTests/QuickSurveyTests.h
@@ -0,0 +1,13 @@
+//
+//  QuickSurveyTests.h
+//  QuickSurveyTests
+//
+//  Created by David Kapp on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuickSurveyTests : SenTestCase
+
+@end
---
+++ QuickSurveyTests/QuickSurveyTests.m
@@ -0,0 +1,32 @@
+//
+//  QuickSurveyTests.m
+//  QuickSurveyTests
+//
+//  Created by David Kapp on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "QuickSurveyTests.h"
+
+@implementation QuickSurveyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QuickSurveyTests");
+}
+
+@end
